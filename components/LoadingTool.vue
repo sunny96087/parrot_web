1d6eb03abaa8b17de1bf0a387c735fe67604f3cb
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-23 00:05:30 +0800

    feat: 新增 loading gif

--- components/LoadingTool.vue
+++ components/LoadingTool.vue
@@ -5,8 +5,11 @@ import { isLoading } from '~/store/eventBus'
 <template>
   <div>
     <div v-if="isLoading" class="spinner-container">
-      <div class=" text-primary">
+      <!-- <div class=" text-primary">
         <span class="">Loading...</span>
+      </div> -->
+      <div class="">
+        <img src="~/assets/images/birdGif.gif" alt="birdGif" />
       </div>
     </div>
   </div>
@@ -20,27 +23,26 @@ import { isLoading } from '~/store/eventBus'
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #eee3fe36;
+  background-color: #28456b82;
 }
 
-.spinner-container {
- position: fixed;
- inset: 0;
- z-index: 1300;
- display: flex;
- justify-content: center;
- align-items: center;
- background-color: #03438d36;
-}
-
-.text-primary {
- color: #03438d !important;
- animation: textJump 1s infinite;
+/* .text-primary {
+  color: #03438d !important;
+  animation: textJump 1s infinite;
 }
 
 @keyframes textJump {
-  0% { opacity: 1; transform: translateY(0); }
-  50% { opacity: 0.5; transform: translateY(-10px); }
-  100% { opacity: 1; transform: translateY(0); }
-}
+  0% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  50% {
+    opacity: 0.5;
+    transform: translateY(-10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+} */
 </style>
--- pages/food.vue
+++ pages/food.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { APIStore } from '~/store/apiService'
+const store = APIStore()
+import { showToast, openDialog, showLoading, hideLoading } from '~/store/eventBus'
+</script>
 <template>
   <div class="w-full">food</div>
 </template>
