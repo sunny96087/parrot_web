9982cc12f3b3ad3b03f544f35713315afed6c95b
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-27 10:45:04 +0800

    feat: set Clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,21 @@ export default defineNuxtConfig({
         },
         { property: 'twitter:title', content: '鸚鸚食堂 Parrot Bistro' }
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      script: [
+        {
+          type: 'text/javascript',
+          innerHTML: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mwgwkagf2w");
+          `,
+          charset: 'utf-8'
+        }
+      ],
+      __dangerouslyDisableSanitizers: ['script']
     }
   },
   plugins: ['~/plugins/vue-gtag.client.ts']
